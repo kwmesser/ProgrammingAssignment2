3eb32013103fc0f3b926737a691ae2714145c83c
Kacy Messerschmidt <kwmesser@gmail.com>
2018-05-23 15:34:56 -0500

    Submission 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Creates an object containing four methods:
+#     set: Assign a matrix A to the object
+#     get: Return the matrix stored in the object
+#     setInv: Assign the inverse of A in the object
+#     getInv: Return the inverse stored in the object
+# Will store a nonsquare or noninvertible matrix
+makeCacheMatrix <- function(A = matrix())
+{
+    # inv is initialized to NULL
+    inv <- NULL
+    
+    set <- function(B)
+    {
+        # When new matrix is stored, inv is set to NULL
+        A <<- B
+        inv <<- NULL
+    }
+    
+    get <- function()
+    {
+        A
+    }
+    
+    setInv <- function(inverse)
+    {
+        # This method will blindly assign any input to inv
+        # Only use cacheSolve to assign inverse
+        inv <<- inverse
+    }
+    
+    getInv <- function()
+    {
+        inv
+    }
+    
+    # Return methods as list
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Returns the inverse of the matrix stored in x
+# Throws error if matrix is not square or not invertible
+cacheSolve <- function(x, ...)
+{
+    # Retrieve previously cached value of inv
+    inv <- x$getInv()
+    
+    if (!is.null(inv))
+    {
+        # If inv is not NULL, use cached value
+        message("Getting cached data")
+        return(inv)
+    }
+    
+    # If inv is NULL, calculate inverse of matrix
+    inv <- solve(x$get(), ...)
+    
+    # Store inv in x
+    x$setInv(inv)
+    
+    # Return inv
+    inv
 }
